4f16d512a3cd5813ae497f09e83522ab79fada5c
dchu0307 <danielchu319@gmail.com>
2025-10-07 09:36:24 -0700

    added 2 shadcn features: tooltip and card

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 import { toast } from "sonner";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -14,25 +16,37 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start w-[60%]">
-        <h1 className="text-2xl font-bold tracking-tight">this is frank</h1>
-        <Image
-          className="w-full h-auto"
-          src="/homepage.JPG"
-          alt="My photo"
-          width={180}
-          height={180}
-          priority
-        />
+        <Card className="w-full h-full">
+          <CardHeader>
+            <CardTitle>this is frank</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Image
+              className="w-full h-auto rounded-lg"
+              src="/homepage.JPG"
+              alt="My photo"
+              width={1200}
+              height={800}
+              priority
+            />
+          </CardContent>
+        </Card>
+        
         <p className="text-sm/6 text-center sm:text-left tracking-[-.01em]">
           keep reading to see more of frank :)
         </p>
         <div className="w-full sm:w-auto">
-          <Button
-            className="mt-2"
-            onClick={() => toast("Hello from Frank!")}
-          >
-            click to say hi
-          </Button>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button
+                className="mt-2"
+                onClick={() => toast("Hello from Frank!")}
+              >
+                click to say hi
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent sideOffset={8}>say hi to frank</TooltipContent>
+          </Tooltip>
         </div>
 
         <h2 className="text-xl font-semibold mt-6">my favorite pictures of frank</h2>
